ab7ba628bb9d8d7fa62bf9a33279a0382b758048
Peter Grenholm <gholm.pet@gmail.com>
2017-03-27 13:21:34 +0100

    Update and rename bilinear_kernel.py to bilinearKernel.py

--- bilinearKernel.py
+++ bilinearKernel.py
@@ -1,17 +1,25 @@
-#This is done in Keras 2.0, with tensorflow channel ordering (channels_last)
+"""
+File: bilinearKernel.py
+
+Author: Peter Grenholm
+
+Utilities for upsampling images in Keras 2.0, with either convolutional or deconvolutional layers.
+"""
+
 import numpy as np
 
-#Use these weights on a Conv2D or a Conv2DTranspose layer in Keras,
-#and you can perform bilinear interpolation by integer magnification (h,w),
-#Arguments: 
-#  '(h,w)' is the magnification factors for height and weight. These must be integers.
-#   If the input image has size (in_height, in_width), the output image after scaling will have size
-#   (h*(in_height-1) + 1,  w*(in_width-1) + 1)
-#   'channels' is the number of channels, this must be the same in input and output image.
-#   'use_bias' must have the same value as 'use_bias' in the Keras layer initialization. Default is True,
-#   which means that the layer has a bias which is set to 0.0.
-#   'dtype' is the datatype of the returned weights
 def bilinear_kernel(h,w,channels, use_bias = True, dtype = "float32") :
+"""
+Returns uniform layer weights for a Conv2D or ConvTranspose2D layer in Keras 2.0, tensorflow channel ordering. 
+If the input image has size (in_height, in_width), the output image after scaling will have size
+(h*(in_height-1) + 1,  w*(in_width-1) + 1)
+Arguments:
+    (h,w) : the magnification factors for height and weight. These must be positive integers.
+    channels : the number of channels/filters, this must be the same in input and output image. Positive integer
+    use_bias : must have the same value as 'use_bias' in the layer initialization. Default is True,
+              which means that the layer has a bias which is set to 0.0. Boolean
+    dtype : the datatype of the returned weights
+"""
     y = np.zeros((h,w,channels,channels), dtype = dtype)
     for i in range(0,h) :
         for j in range(0,w) :
@@ -19,7 +27,9 @@ def bilinear_kernel(h,w,channels, use_bias = True, dtype = "float32") :
     if use_bias : return [y,np.array([0.], dtype = dtype)]
     else : return [y]
     
-#Examples:
+"""
+Examples:
+"""
 
 from keras.models import Model
 in_height, in_width = 32,32
@@ -33,7 +43,7 @@ us = UpSampling2D(size = (h,w))(input_layer)
 conv = Conv2D(filters = channels, kernel_size = (h,w), strides=(1,1), 
             activation = 'linear',
             padding = 'valid', name = 'conv', use_bias = False,
-            weights = bilinear_kernel(h,w,channels, False))(us)
+            weights = bilinearKernel(h,w,channels, False))(us)
 model = Model(input_layer, conv)
 
 #A transposed convolutional layer will need extra cropping 
@@ -45,7 +55,7 @@ us = UpSampling2D(size = (h,w))(input_layer)
 ct = Conv2DTranspose(filters = channels, kernel_size = (h,w),strides=(1,1), 
             activation = 'linear',
             padding = 'valid', name = 'ct', 
-            weights = bilinear_kernel(h,w,channels))(us)
+            weights = bilinearKernel(h,w,channels))(us)
 cr = Cropping2D(cropping = (h-1,w-1))(ct)
 model = Model(input_layer, cr)
 
